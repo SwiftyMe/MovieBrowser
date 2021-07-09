cedf8bd57d9c0f722f375d245c23531f3aa8ea44
Anders Sommer Lassen <anders.lassen@icloud.com>
2021-07-09 11:58:16 +0200

    Initial Commit

---
+++ PopularMovies/PopularMoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  PopularMoviesApp.swift
+//  PopularMovies
+//
+//  Created by Anders Lassen on 09/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct PopularMoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
